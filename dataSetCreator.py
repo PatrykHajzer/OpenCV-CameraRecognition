94bdf3d19db12faf0b917dfbecfc9be77f24068f
PatrykHajzer <hajzer.patryk@gmail.com>
2017-06-14 12:50:03 +0200

    Create dataSetCreator.py

--- dataSetCreator.py
+++ dataSetCreator.py
@@ -11,16 +11,15 @@ while(True):
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         
-        #incrementing sample number 
         sampleNum=sampleNum+1
-        #saving the captured face in the dataset folder
+
         cv2.imwrite("dataSet/User."+Id +'.'+ str(sampleNum) + ".jpg", gray[y:y+h,x:x+w])
 
         cv2.imshow('frame',img)
-    #wait for 100 miliseconds 
+
     if cv2.waitKey(100) & 0xFF == ord('q'):
         break
-    # break if the sample number is morethan 20
+
     elif sampleNum>20:
         break
 cam.release()
